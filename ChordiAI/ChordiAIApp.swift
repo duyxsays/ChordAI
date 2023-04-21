196d329dc0c1a855980b7ec120cc54306a39a21b
Simon Andersen <sandersen98@live.dk>
2023-04-21 10:56:50 +0200

    Initial Commit

---
+++ ChordiAI/ChordiAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordiAIApp.swift
+//  ChordiAI
+//
+//  Created by Simon Andersen on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChordiAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
